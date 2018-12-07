1f74f1886cb012c60439043ff819f3ce38d6d766
RocGit <roc.woods2009@gmail.com>
2018-12-07 11:42:47 +0800

    a comment demo exec is added

--- src/Gallery/Comment/CommentApp.js
+++ src/Gallery/Comment/CommentApp.js
@@ -3,11 +3,30 @@ import CommentInput from './CommentInput'
 import CommentList from './CommentList'
 
 class CommentApp extends Component {
+  // 我们在 CommentApp 的 state 中初始化一个数组，来保存所有的评论数据，并且通过 props 把它传递给 CommentList
+  constructor() {
+    super()
+    this.state = {
+      comments: []
+    }
+  }
+
+  handleSubmitComment(comment) {
+    console.log(comment)
+    alert(comment.username + "\n" + comment.content)
+
+    this.state.comments.push(comment)
+    this.setState({
+      comments: this.state.comments
+    })
+  }
+
   render() {
     return (
       <div className='wrapper'>
-        <CommentInput />
-        <CommentList />
+        <CommentInput
+          onSubmit={this.handleSubmitComment.bind(this)} />
+        <CommentList comments={this.state.comments} />
       </div>
     )
   }
